b21177a2f3feb40ab35202e76f7eed0c6e292046
Etsu <etsu4296@gmail.com>
2016-10-03 16:37:49 +0400

    code refactoring

--- NC DecToAll.py
+++ NC DecToAll.py
@@ -1,125 +1,122 @@
+# -*- coding: utf-8 -*-
 
-def DecToBin(num):
-    IntPart = int(num)
-    FloatPart = num - int(num)
+def dec_to_bin(num):
+    int_part = int(num)
+    float_part = num - int(num)
 
-    print(IntPart,FloatPart) #отладка
-    
-    BinIntPart = 0
-    BinFloatPart = 0
-    temp1 = 0
+    bin_int_part = 0
+    bin_float_part = 0
     i = 1
     j = 0.1
-    
-    while IntPart > 0:
-        BinIntPart = BinIntPart + IntPart % 2 * i
+
+    while int_part > 0:
+        bin_int_part = bin_int_part + int_part % 2 * i
         i = i * 10
-        IntPart = int(IntPart / 2)
+        int_part = int(int_part / 2)
 
     while j > 0.00000001:
-        temp1 = FloatPart * 2
-        BinFloatPart = BinFloatPart + (int(temp1)) * j
-        FloatPart = temp1 - int(temp1)
+        tmp = float_part * 2
+        bin_float_part = bin_float_part + (int(tmp)) * j
+        float_part = tmp - int(tmp)
         j = j / 10
 
-    BinFull = BinIntPart + BinFloatPart #возможно не нужно
-    print('{:.8f}'.format(BinFull)) #отладка
-    
-    return BinIntPart, BinFloatPart
+    bin_full = bin_int_part + bin_float_part
+    print('Bin Full: ''{:.8f}'.format(bin_full))
+
+    return bin_int_part, bin_float_part
 
 
-def BinToHex(HexInt, HexFloat):
-    dic = {'0000':'0','1000':'8',
-           '0001':'1','1001':'9',
-           '0010':'2','1010':'A',
-           '0011':'3','1011':'B',
-           '0100':'4','1100':'C',
-           '0101':'5','1101':'D',
-           '0110':'6','1110':'E',
-           '0111':'7','1111':'F'}
-    
-    print (str(HexInt), str(HexFloat)) #отладка
-    HexInt = str(HexInt)
-    if (len(HexInt)%4 != 0):
-        HexInt = HexInt.rjust(8, '0') #корректировка, сделать динамически
+def bin_to_hex(hex_int, hex_float):
+    dic = {'0000': '0', '1000': '8',
+           '0001': '1', '1001': '9',
+           '0010': '2', '1010': 'A',
+           '0011': '3', '1011': 'B',
+           '0100': '4', '1100': 'C',
+           '0101': '5', '1101': 'D',
+           '0110': '6', '1110': 'E',
+           '0111': '7', '1111': 'F'}
 
-    HexFull = ''    
-    cut1=0
-    cut2=4
-    while(cut2<=len(HexInt)):
-        temp1 = HexInt[cut1:cut2]
+    print ('Int Part: ', hex_int)
+    print ('Float Part: ', hex_float)
+    hex_int = str(hex_int)
+    while (len(hex_int) % 4 != 0):
+        hex_int = '0' + hex_int
+
+    hex_full = ''
+    cut1 = 0
+    cut2 = 4
+    while (len(hex_int) >= cut2):
+        temp = hex_int[cut1:cut2]
         for key in dic.keys():
-            temp1 = temp1.replace(key, dic[key])
+            temp = temp.replace(key, dic[key])
         cut1 += 4
         cut2 += 4
-        HexFull += temp1
+        hex_full += temp
 
-    HexFloat = str(HexFloat)[2:]
-    if (len(HexFloat)%4 != 0):
-        HexFloat = HexFloat.ljust(8, '0') #корректировка, сделать динамически
+    hex_float = str(hex_float)[2:]
+    while (len(hex_float) % 4 != 0):
+        hex_float = hex_float + '0'
 
-    HexFull += '.'
-    
-    cut1=0
-    cut2=4
-    while(cut2<=len(HexFloat)):
-        temp1 = HexFloat[cut1:cut2]
+    hex_full += '.'
+
+    cut1 = 0
+    cut2 = 4
+    while (len(hex_float) >= cut2):
+        temp = hex_float[cut1:cut2]
         for key in dic.keys():
-            temp1 = temp1.replace(key, dic[key])
+            temp = temp.replace(key, dic[key])
         cut1 += 4
         cut2 += 4
-        HexFull += temp1
+        hex_full += temp
 
-    print(HexFull) #отладка   
-    
-    return HexFull
+    return hex_full
 
-def BinToOct(OctInt, OctFloat):
-    dic = {'000':'0','100':'4',
-           '001':'1','101':'5',
-           '010':'2','110':'6',
-           '011':'3','111':'7'}
-    
-    OctInt = str(OctInt)
-    if (len(OctInt)%3 != 0):
-        OctInt = OctInt.rjust(9, '0') #корректировка, сделать динамически
 
-    OctFull = ''    
-    cut1=0
-    cut2=3
-    while(cut2<=len(OctInt)):
-        temp1 = OctInt[cut1:cut2]
+def bin_to_oct(oct_int, oct_float):
+    dic = {'000': '0', '100': '4',
+           '001': '1', '101': '5',
+           '010': '2', '110': '6',
+           '011': '3', '111': '7'}
+
+    oct_int = str(oct_int)
+    while (len(oct_int) % 3 != 0):
+        oct_int = '0' + oct_int
+
+    oct_full = ''
+    cut1 = 0
+    cut2 = 3
+    while (len(oct_int) >= cut2):
+        temp = oct_int[cut1:cut2]
         for key in dic.keys():
-            temp1 = temp1.replace(key, dic[key])
+            temp = temp.replace(key, dic[key])
         cut1 += 3
         cut2 += 3
-        OctFull += temp1
+        oct_full += temp
 
-    OctFloat = str(OctFloat)[2:]
-    if (len(OctFloat)%3 != 0):
-        OctFloat = OctFloat.ljust(9, '0') #корректировка, сделать динамически
+    oct_float = str(oct_float)[2:]
+    while (len(oct_float) % 3 != 0):
+        oct_float = oct_float + '0'
+    oct_full += '.'
 
-    OctFull += '.'
-    
-    cut1=0
-    cut2=3
-    while(cut2<=len(OctFloat)):
-        temp1 = OctFloat[cut1:cut2]
+    cut1 = 0
+    cut2 = 3
+    while (cut2 <= len(oct_float)):
+        temp = oct_float[cut1:cut2]
         for key in dic.keys():
-            temp1 = temp1.replace(key, dic[key])
+            temp = temp.replace(key, dic[key])
         cut1 += 3
         cut2 += 3
-        OctFull += temp1
+        oct_full += temp
 
-    print(OctFull) #отладка   
-    
-    return OctFull
+    print('Oct Full: ', oct_full)
+
+    return oct_full
 
 
-HexInt, HexFloat = DecToBin(255.6541)
-BinToHex(HexInt, HexFloat)
-BinToOct(HexInt, HexFloat)
-print()
-HexInt, HexFloat = DecToBin(30)
-BinToHex(HexInt, HexFloat)
-BinToOct(HexInt, HexFloat)
+hex_int, hex_float = dec_to_bin(255.6541)
+bin_to_hex(hex_int, hex_float)
+bin_to_oct(hex_int, hex_float)
+print('===========================')
+hex_int, hex_float = dec_to_bin(30)
+bin_to_hex(hex_int, hex_float)
+bin_to_oct(hex_int, hex_float)
